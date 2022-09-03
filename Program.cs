6e29179ab9dbae76c4b5ccf24a85cd8e806e1eef
Karina Babakova <taytynovak@mail.ru>
2022-09-03 15:37:25 +0700

    square

--- Program.cs
+++ Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine ("Введите число ");
+string input; //ввод пользователя
+input = Console.ReadLine();
+
+int number = int.Parse(input);
+
+int square = number*number;
+
+Console.WriteLine($"Число в квадрате равно {square} ");
